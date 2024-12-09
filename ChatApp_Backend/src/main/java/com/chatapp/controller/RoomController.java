7b96efac3a6265dddc878a19643d8a97a137c1e3
hritikchauhanji <hritikchji@gmail.com>
2024-12-09 21:31:55 +0530

    room controller

---
+++ ChatApp_Backend/src/main/java/com/chatapp/controller/RoomController.java
@@ -0,0 +1,74 @@
+package com.chatapp.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.chatapp.entities.Message;
+import com.chatapp.entities.Room;
+import com.chatapp.repository.RoomRepository;
+@RestController
+@RequestMapping("api/v1/rooms")
+public class RoomController {
+	
+	@Autowired
+	private RoomRepository roomRepository;
+
+	@PostMapping("/create")
+	ResponseEntity<?> createRoom(@RequestBody String roomId) {
+		if (roomRepository.findByRoomId(roomId) != null) {
+			//room is already exists
+			return ResponseEntity.badRequest().body("room is already exist");
+		}
+		
+		// create new room
+		
+		Room room = new Room();
+		room.setRoomId(roomId);
+		roomRepository.save(room);
+		return ResponseEntity.status(HttpStatus.CREATED).body(room);
+	}
+	
+	@GetMapping("{/roomId}")
+	ResponseEntity<?> joinRoom(@PathVariable String roomId) {
+		
+		Room room = roomRepository.findByRoomId(roomId);
+		
+		if(room == null) {
+			return ResponseEntity.badRequest().body("room is not found");
+		}
+		
+		return ResponseEntity.ok(room);
+	}
+	
+	//get message in room
+	@GetMapping("/{roomId}/messages")
+	ResponseEntity<?> getMessages(
+			@PathVariable String roomId,
+			@RequestParam(value = "page" , defaultValue = "0" , required = false) int page,
+			@RequestParam(value = "size", defaultValue = "20", required = false) int size
+			) {
+		Room room = roomRepository.findByRoomId(roomId);
+		
+		if(room == null) {
+			return ResponseEntity.badRequest().build();
+		}
+		
+		//get messages 
+		
+		//pagination
+		
+		List<Message> messages = new ArrayList<>();
+		return ResponseEntity.ok(messages);
+	}
+}
