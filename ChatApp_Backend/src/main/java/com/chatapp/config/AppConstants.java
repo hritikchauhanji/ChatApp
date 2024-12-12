0f52d4a86a5e0fa7631cfd6c6097ddb58618c436
hritikchauhanji <hritikchji@gmail.com>
2024-12-12 19:12:40 +0530

    udd

--- ChatApp_Backend/src/main/java/com/chatapp/config/AppConstants.java
+++ ChatApp_Backend/src/main/java/com/chatapp/config/AppConstants.java
@@ -2,5 +2,5 @@ package com.chatapp.config;
 
 public class AppConstants {
 
-	public static final String Frontend_Base_URL="http://localhost:5173";
+	public static final String Frontend_Base_URL="https://chatapp-9u0z.onrender.com";
 }
