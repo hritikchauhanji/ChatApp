125b8c1d8468d6eb398e1450b7f376d20d7f82e1
hritikchauhanji <hritikchji@gmail.com>
2024-12-28 15:12:22 +0530

    update database mongo to postgresql

--- ChatApp-Frontend/src/config/AxiosHelper.js
+++ ChatApp-Frontend/src/config/AxiosHelper.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-export const baseUrl = "https://chatapp-production-d49a.up.railway.app";
+// export const baseUrl = "https://chatapp-production-d49a.up.railway.app";
+export const baseUrl = "http://localhost:8080";
 export const httpClient = axios.create({
   baseURL: baseUrl,
 });
--- ChatApp_Backend/src/main/java/com/chatapp/entities/Room.java
+++ ChatApp_Backend/src/main/java/com/chatapp/entities/Room.java
@@ -2,26 +2,46 @@ package com.chatapp.entities;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
 
+//import org.springframework.data.mongodb.core.mapping.Document;
+
+import jakarta.persistence.CascadeType;
+//import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+//import jakarta.persistence.FetchType;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Document(collection = "rooms")
+//@Document(collection = "rooms")
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "rooms")
 public class Room {
 
 	@Id
-	private String id;
-	
+	private String id; /*= UUID.randomUUID().toString(); Generate a unique ID*/
+
 	private String roomId;
-	
+
+	@OneToMany(cascade = CascadeType.ALL,orphanRemoval = true, fetch = FetchType.EAGER) // Persist messages with the Room
 	private List<Message> messages = new ArrayList<>();
+
+	@PrePersist
+	public void prePersist() {
+		if (this.id == null || this.id.isEmpty()) {
+			this.id = UUID.randomUUID().toString();
+		}
+	}
 }
