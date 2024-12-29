4b07ac203805cd7492be6661aa2bf0c37f6bdf46
hritikchauhanji <hritikchji@gmail.com>
2024-12-29 14:09:23 +0530

    postgresql_prod

--- ChatApp-Frontend/src/config/AxiosHelper.js
+++ ChatApp-Frontend/src/config/AxiosHelper.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-// export const baseUrl = "https://chatapp-production-d49a.up.railway.app";
-export const baseUrl = "http://localhost:8080";
+export const baseUrl = "https://chatapp-backend-latest.onrender.com";
+// export const baseUrl = "http://localhost:8080";
 export const httpClient = axios.create({
   baseURL: baseUrl,
 });
